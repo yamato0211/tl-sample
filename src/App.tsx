196e2e3ac2f521356cf4b58933598f825676be30
yamato0211 <kumayama0211@gmail.com>
2024-03-13 02:04:39 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Auth from './Auth';
 import Timeline from './Timeline';
